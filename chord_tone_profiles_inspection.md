370429579ee5d52fa708f0d162724006f731bb75
Johannes Hentschel <johannes.hentschel@epfl.ch>
2024-01-24 15:45:50 +0100

    integrates the 9 compared features into chord_tone_profiles_inspection.md

--- chord_tone_profiles_inspection.md
+++ chord_tone_profiles_inspection.md
@@ -5,7 +5,7 @@ jupytext:
     extension: .md
     format_name: myst
     format_version: 0.13
-    jupytext_version: 1.16.0
+    jupytext_version: 1.16.1
 kernelspec:
   display_name: revamp
   language: python
@@ -14,7 +14,7 @@ kernelspec:
 
 # Chord Profiles
 
-```{code-cell}
+```{code-cell} ipython3
 ---
 mystnb:
   code_prompt_hide: Hide imports
@@ -24,6 +24,7 @@ tags: [hide-cell]
 %load_ext autoreload
 %autoreload 2
 import os
+from typing import Dict, List, Tuple
 
 import dimcat as dc
 import ms3
@@ -41,7 +42,7 @@ pd.set_option("display.max_rows", 1000)
 pd.set_option("display.max_columns", 500)
 ```
 
-```{code-cell}
+```{code-cell} ipython3
 RESULTS_PATH = os.path.expanduser("~/git/diss/31_profiles/figs")
 os.makedirs(RESULTS_PATH, exist_ok=True)
 
@@ -61,7 +62,7 @@ def save_figure_as(fig, filename, directory=RESULTS_PATH, **kwargs):
     write_image(fig, filename, directory, **kwargs)
 ```
 
-```{code-cell}
+```{code-cell} ipython3
 :tags: [hide-input]
 
 package_path = utils.resolve_dir(
@@ -76,30 +77,163 @@ D = dc.Dataset.from_package(package_path)
 D
 ```
 
-```{code-cell}
+```{code-cell} ipython3
 chord_slices = utils.get_sliced_notes(D)
 chord_slices.head(5)
 ```
 
-## Document frequencies of chord features
+```{code-cell} ipython3
+features = {
+    "root_per_globalkey": (  # baseline globalkey-roots without any note information
+        ["root_per_globalkey", "intervals_over_root"],
+        "chord symbols (root per globalkey + intervals)",
+    ),
+    "root_per_localkey": (  # baseline localkey-roots without any note information
+        ["root", "intervals_over_root"],
+        "chord symbols (root per localkey + intervals)",
+    ),
+    "root_per_tonicization": (  # baseline root over tonicized key without any note information
+        ["root_per_tonicization", "intervals_over_root"],
+        "chord symbols (root per tonicization + intervals)",
+    ),
+    "globalkey_profiles": (  # baseline notes - globalkey
+        ["fifths_over_global_tonic"],
+        "tone profiles as per global key",
+    ),
+    "localkey_profiles": (  # baseline notes - localkey
+        ["fifths_over_local_tonic"],
+        "tone profiles as per local key",
+    ),
+    "tonicization_profiles": (  # baseline notes - tonicized key
+        ["fifths_over_tonicization"],
+        "tone profiles as per tonicized key",
+    ),
+    "global_root_ct": (
+        ["root_per_globalkey", "fifths_over_root"],
+        "chord-tone profiles over root-per-globalkey",
+    ),
+    "local_root_ct": (
+        ["root", "fifths_over_root"],
+        "chord-tone profiles over root-per-localkey",
+    ),
+    "tonicization_root_ct": (
+        [
+            "root_per_tonicization",
+            "fifths_over_root",
+        ],
+        "chord-tone profiles over root-per-tonicization",
+    ),
+}
 
-```{code-cell}
-utils.compare_corpus_frequencies(
-    chord_slices,
-    [
-        "chord_reduced_and_mode",
-        ["effective_localkey_is_minor", "numeral"],
-        "root",
-        "root_per_globalkey",
-    ],
+
+analyzer_config = dc.DimcatConfig(
+    "PrevalenceAnalyzer",
+    index=["corpus", "piece"],
 )
+
+
+def make_data(
+    chord_slices: resources.DimcatResource,
+    features: Dict[str, Tuple[str | List[str], str]],
+) -> Dict[str, resources.PrevalenceMatrix]:
+    data = {}
+    for feature_name, (feature_columns, info) in features.items():
+        print(f"Computing prevalence matrix for {info}")
+        analyzer_config.update(columns=feature_columns)
+        prevalence_matrix = chord_slices.apply_step(analyzer_config)
+        data[feature_name] = prevalence_matrix
+    return data
+
+
+data = make_data(chord_slices, features)
 ```
 
+## Document frequencies of chord features
+
+```{code-cell} ipython3
+info2features = {key: feature_columns for feature_columns, key in features.values()}
+ranking_table = utils.compare_corpus_frequencies(chord_slices, info2features)
+ranking_table.columns.rename("feature", level=0, inplace=True)
+ranking_table.index = ranking_table.index.rename("rank") + 1
+ranking_table.iloc[:30]
+```
+
++++ {"jupyter": {"outputs_hidden": false}}
+
+## Principal Component Analyses
+### Chord profiles
+
+```{code-cell} ipython3
+---
+jupyter:
+  outputs_hidden: false
+---
+metadata = D.get_metadata()
+PIECE_YEARS = metadata.get_composition_years().rename("mean_composition_year")
+PIECE_MODE = metadata.annotated_key.str.islower().map({True: "minor", False: "major"})
+
+
+def show_pca(feature, **kwargs):
+    global data, features
+    prevalence_matrix = data[feature]
+    info = features[feature][1]
+    return utils.plot_pca(prevalence_matrix.relative, info=info, **kwargs)
+
+
+show_pca("root_per_globalkey", color=PIECE_YEARS, symbol=PIECE_MODE)
+```
+
+```{code-cell} ipython3
+show_pca("root_per_localkey", color=PIECE_YEARS, symbol=PIECE_MODE)
+```
+
+```{code-cell} ipython3
+show_pca("root_per_tonicization", color=PIECE_YEARS, symbol=PIECE_MODE)
+```
+
+## Pitch-class profiles
+
+```{code-cell} ipython3
+show_pca("globalkey_profiles", color=PIECE_YEARS, symbol=PIECE_MODE)
+```
+
+```{code-cell} ipython3
+show_pca("localkey_profiles", color=PIECE_YEARS, symbol=PIECE_MODE)
+```
+
+```{code-cell} ipython3
+show_pca("tonicization_profiles", color=PIECE_YEARS, symbol=PIECE_MODE)
+```
+
+### Chord-tone profiles
+
+```{code-cell} ipython3
+show_pca("global_root_ct", color=PIECE_YEARS, symbol=PIECE_MODE)
+```
+
+```{code-cell} ipython3
+show_pca("local_root_ct", color=PIECE_YEARS, symbol=PIECE_MODE)
+```
+
+```{code-cell} ipython3
+show_pca("tonicization_root_ct", color=PIECE_YEARS, symbol=PIECE_MODE)
+```
+
+```{raw-cell}
+---
+jupyter:
+  outputs_hidden: false
+---
 ## Create chord-tone profiles for multiple chord features
 
 Tokens are `(feature, ..., chord_tone)` tuples.
+```
 
-```{code-cell}
+```{raw-cell}
+---
+jupyter:
+  outputs_hidden: false
+---
 chord_reduced: resources.PrevalenceMatrix = chord_slices.apply_step(
     dc.DimcatConfig(
         "PrevalenceAnalyzer",
@@ -108,9 +242,14 @@ chord_reduced: resources.PrevalenceMatrix = chord_slices.apply_step(
     )
 )
 print(f"Shape: {chord_reduced.shape}")
+utils.replace_boolean_column_level_with_mode(chord_reduced)
 ```
 
-```{code-cell}
+```{raw-cell}
+---
+jupyter:
+  outputs_hidden: false
+---
 numerals: resources.PrevalenceMatrix = chord_slices.apply_step(
     dc.DimcatConfig(
         "PrevalenceAnalyzer",
@@ -122,7 +261,11 @@ print(f"Shape: {numerals.shape}")
 utils.replace_boolean_column_level_with_mode(numerals)
 ```
 
-```{code-cell}
+```{raw-cell}
+---
+jupyter:
+  outputs_hidden: false
+---
 roots: resources.PrevalenceMatrix = chord_slices.apply_step(
     dc.DimcatConfig(
         "PrevalenceAnalyzer",
@@ -133,7 +276,11 @@ roots: resources.PrevalenceMatrix = chord_slices.apply_step(
 print(f"Shape: {roots.shape}")
 ```
 
-```{code-cell}
+```{raw-cell}
+---
+jupyter:
+  outputs_hidden: false
+---
 root_per_globalkey = chord_slices.apply_step(
     dc.DimcatConfig(
         "PrevalenceAnalyzer",
@@ -144,35 +291,63 @@ root_per_globalkey = chord_slices.apply_step(
 print(f"Shape: {root_per_globalkey.shape}")
 ```
 
-### Document frequencies of the tokens
-
-```{code-cell}
+```{raw-cell}
+---
+jupyter:
+  outputs_hidden: false
+---
 fig = utils.plot_document_frequency(chord_reduced)
 save_figure_as(fig, "document_frequency_of_chord_tones")
 fig
 ```
 
-```{code-cell}
+```{raw-cell}
+---
+jupyter:
+  outputs_hidden: false
+---
 utils.plot_document_frequency(numerals, info="numerals")
 ```
 
-```{code-cell}
+```{raw-cell}
+---
+jupyter:
+  outputs_hidden: false
+---
 utils.plot_document_frequency(roots, info="roots")
 ```
 
-```{code-cell}
+```{raw-cell}
+---
+jupyter:
+  outputs_hidden: false
+---
 utils.plot_document_frequency(
     root_per_globalkey, info="root relative to global tonic"
 )
 ```
 
+```{raw-cell}
+---
+jupyter:
+  outputs_hidden: false
+---
 ## Principal Component Analyses
+```
 
-```{code-cell}
+```{raw-cell}
+---
+jupyter:
+  outputs_hidden: false
+---
 # chord_reduced.query("piece in ['op03n12a', 'op03n12b']").dropna(axis=1, how='all')
 ```
 
-```{code-cell}
+```{raw-cell}
+---
+jupyter:
+  outputs_hidden: false
+---
 metadata = D.get_metadata()
 CORPUS_YEARS = utils.corpus_mean_composition_years(metadata)
 PIECE_YEARS = metadata.get_composition_years().rename("mean_composition_year")
@@ -183,7 +358,11 @@ utils.plot_pca(
 )
 ```
 
-```{code-cell}
+```{raw-cell}
+---
+jupyter:
+  outputs_hidden: false
+---
 utils.plot_pca(
     chord_reduced.combine_results("corpus").relative,
     info="chord-tone profiles of reduced chords",
@@ -192,13 +371,21 @@ utils.plot_pca(
 )
 ```
 
-```{code-cell}
+```{raw-cell}
+---
+jupyter:
+  outputs_hidden: false
+---
 utils.plot_pca(
     numerals.relative, info="numeral profiles of numerals", color=PIECE_YEARS
 )
 ```
 
-```{code-cell}
+```{raw-cell}
+---
+jupyter:
+  outputs_hidden: false
+---
 utils.plot_pca(
     numerals.combine_results("corpus").relative,
     info="chord-tone profiles of numerals",
@@ -207,13 +394,21 @@ utils.plot_pca(
 )
 ```
 
-```{code-cell}
+```{raw-cell}
+---
+jupyter:
+  outputs_hidden: false
+---
 utils.plot_pca(
     roots.relative, info="root profiles of chord roots (local)", color=PIECE_YEARS
 )
 ```
 
-```{code-cell}
+```{raw-cell}
+---
+jupyter:
+  outputs_hidden: false
+---
 utils.plot_pca(
     roots.combine_results("corpus").relative,
     info="chord-tone profiles of chord roots (local)",
@@ -222,7 +417,11 @@ utils.plot_pca(
 )
 ```
 
-```{code-cell}
+```{raw-cell}
+---
+jupyter:
+  outputs_hidden: false
+---
 utils.plot_pca(
     root_per_globalkey.relative,
     info="root profiles of chord roots (global)",
@@ -230,7 +429,11 @@ utils.plot_pca(
 )
 ```
 
-```{code-cell}
+```{raw-cell}
+---
+jupyter:
+  outputs_hidden: false
+---
 utils.plot_pca(
     root_per_globalkey.combine_results("corpus").relative,
     info="chord-tone profiles of chord roots (global)",
--- chord_tone_profiles_inspection.py
+++ chord_tone_profiles_inspection.py
@@ -6,7 +6,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.16.0
+#       jupytext_version: 1.16.1
 #   kernelspec:
 #     display_name: revamp
 #     language: python
@@ -20,6 +20,7 @@
 # %load_ext autoreload
 # %autoreload 2
 import os
+from typing import Dict, List, Tuple
 
 import dimcat as dc
 import ms3
@@ -72,144 +73,254 @@ D
 chord_slices = utils.get_sliced_notes(D)
 chord_slices.head(5)
 
+# %%
+features = {
+    "root_per_globalkey": (  # baseline globalkey-roots without any note information
+        ["root_per_globalkey", "intervals_over_root"],
+        "chord symbols (root per globalkey + intervals)",
+    ),
+    "root_per_localkey": (  # baseline localkey-roots without any note information
+        ["root", "intervals_over_root"],
+        "chord symbols (root per localkey + intervals)",
+    ),
+    "root_per_tonicization": (  # baseline root over tonicized key without any note information
+        ["root_per_tonicization", "intervals_over_root"],
+        "chord symbols (root per tonicization + intervals)",
+    ),
+    "globalkey_profiles": (  # baseline notes - globalkey
+        ["fifths_over_global_tonic"],
+        "tone profiles as per global key",
+    ),
+    "localkey_profiles": (  # baseline notes - localkey
+        ["fifths_over_local_tonic"],
+        "tone profiles as per local key",
+    ),
+    "tonicization_profiles": (  # baseline notes - tonicized key
+        ["fifths_over_tonicization"],
+        "tone profiles as per tonicized key",
+    ),
+    "global_root_ct": (
+        ["root_per_globalkey", "fifths_over_root"],
+        "chord-tone profiles over root-per-globalkey",
+    ),
+    "local_root_ct": (
+        ["root", "fifths_over_root"],
+        "chord-tone profiles over root-per-localkey",
+    ),
+    "tonicization_root_ct": (
+        [
+            "root_per_tonicization",
+            "fifths_over_root",
+        ],
+        "chord-tone profiles over root-per-tonicization",
+    ),
+}
+
+
+analyzer_config = dc.DimcatConfig(
+    "PrevalenceAnalyzer",
+    index=["corpus", "piece"],
+)
+
+
+def make_data(
+    chord_slices: resources.DimcatResource,
+    features: Dict[str, Tuple[str | List[str], str]],
+) -> Dict[str, resources.PrevalenceMatrix]:
+    data = {}
+    for feature_name, (feature_columns, info) in features.items():
+        print(f"Computing prevalence matrix for {info}")
+        analyzer_config.update(columns=feature_columns)
+        prevalence_matrix = chord_slices.apply_step(analyzer_config)
+        data[feature_name] = prevalence_matrix
+    return data
+
+
+data = make_data(chord_slices, features)
+
 # %% [markdown]
 # ## Document frequencies of chord features
 
 # %%
-utils.compare_corpus_frequencies(
-    chord_slices,
-    [
-        "chord_reduced_and_mode",
-        ["effective_localkey_is_minor", "numeral"],
-        "root",
-        "root_per_globalkey",
-    ],
-)
+info2features = {key: feature_columns for feature_columns, key in features.values()}
+ranking_table = utils.compare_corpus_frequencies(chord_slices, info2features)
+ranking_table.columns.rename("feature", level=0, inplace=True)
+ranking_table.index = ranking_table.index.rename("rank") + 1
+ranking_table.iloc[:30]
+
+# %% [markdown] jupyter={"outputs_hidden": false}
+# ## Principal Component Analyses
+# ### Chord profiles
+
+# %% jupyter={"outputs_hidden": false}
+metadata = D.get_metadata()
+PIECE_YEARS = metadata.get_composition_years().rename("mean_composition_year")
+PIECE_MODE = metadata.annotated_key.str.islower().map({True: "minor", False: "major"})
+
+
+def show_pca(feature, **kwargs):
+    global data, features
+    prevalence_matrix = data[feature]
+    info = features[feature][1]
+    return utils.plot_pca(prevalence_matrix.relative, info=info, **kwargs)
+
+
+show_pca("root_per_globalkey", color=PIECE_YEARS, symbol=PIECE_MODE)
+
+# %%
+show_pca("root_per_localkey", color=PIECE_YEARS, symbol=PIECE_MODE)
+
+# %%
+show_pca("root_per_tonicization", color=PIECE_YEARS, symbol=PIECE_MODE)
 
 # %% [markdown]
+# ## Pitch-class profiles
+
+# %%
+show_pca("globalkey_profiles", color=PIECE_YEARS, symbol=PIECE_MODE)
+
+# %%
+show_pca("localkey_profiles", color=PIECE_YEARS, symbol=PIECE_MODE)
+
+# %%
+show_pca("tonicization_profiles", color=PIECE_YEARS, symbol=PIECE_MODE)
+
+# %% [markdown]
+# ### Chord-tone profiles
+
+# %%
+show_pca("global_root_ct", color=PIECE_YEARS, symbol=PIECE_MODE)
+
+# %%
+show_pca("local_root_ct", color=PIECE_YEARS, symbol=PIECE_MODE)
+
+# %%
+show_pca("tonicization_root_ct", color=PIECE_YEARS, symbol=PIECE_MODE)
+
+# %% [raw] jupyter={"outputs_hidden": false}
 # ## Create chord-tone profiles for multiple chord features
 #
 # Tokens are `(feature, ..., chord_tone)` tuples.
 
-# %%
-chord_reduced: resources.PrevalenceMatrix = chord_slices.apply_step(
-    dc.DimcatConfig(
-        "PrevalenceAnalyzer",
-        columns=["chord_reduced_and_mode", "fifths_over_local_tonic"],
-        index=["corpus", "piece"],
-    )
-)
-print(f"Shape: {chord_reduced.shape}")
+# %% [raw] jupyter={"outputs_hidden": false}
+# chord_reduced: resources.PrevalenceMatrix = chord_slices.apply_step(
+#     dc.DimcatConfig(
+#         "PrevalenceAnalyzer",
+#         columns=["chord_reduced_and_mode", "fifths_over_local_tonic"],
+#         index=["corpus", "piece"],
+#     )
+# )
+# print(f"Shape: {chord_reduced.shape}")
+# utils.replace_boolean_column_level_with_mode(chord_reduced)
 
-# %%
-numerals: resources.PrevalenceMatrix = chord_slices.apply_step(
-    dc.DimcatConfig(
-        "PrevalenceAnalyzer",
-        columns=["effective_localkey_is_minor", "numeral", "fifths_over_local_tonic"],
-        index=["corpus", "piece"],
-    )
-)
-print(f"Shape: {numerals.shape}")
-utils.replace_boolean_column_level_with_mode(numerals)
+# %% [raw] jupyter={"outputs_hidden": false}
+# numerals: resources.PrevalenceMatrix = chord_slices.apply_step(
+#     dc.DimcatConfig(
+#         "PrevalenceAnalyzer",
+#         columns=["effective_localkey_is_minor", "numeral", "fifths_over_local_tonic"],
+#         index=["corpus", "piece"],
+#     )
+# )
+# print(f"Shape: {numerals.shape}")
+# utils.replace_boolean_column_level_with_mode(numerals)
 
-# %%
-roots: resources.PrevalenceMatrix = chord_slices.apply_step(
-    dc.DimcatConfig(
-        "PrevalenceAnalyzer",
-        columns=["root", "fifths_over_local_tonic"],
-        index=["corpus", "piece"],
-    )
-)
-print(f"Shape: {roots.shape}")
+# %% [raw] jupyter={"outputs_hidden": false}
+# roots: resources.PrevalenceMatrix = chord_slices.apply_step(
+#     dc.DimcatConfig(
+#         "PrevalenceAnalyzer",
+#         columns=["root", "fifths_over_local_tonic"],
+#         index=["corpus", "piece"],
+#     )
+# )
+# print(f"Shape: {roots.shape}")
 
-# %%
-root_per_globalkey = chord_slices.apply_step(
-    dc.DimcatConfig(
-        "PrevalenceAnalyzer",
-        columns=["root_per_globalkey", "fifths_over_local_tonic"],
-        index=["corpus", "piece"],
-    )
-)
-print(f"Shape: {root_per_globalkey.shape}")
+# %% [raw] jupyter={"outputs_hidden": false}
+# root_per_globalkey = chord_slices.apply_step(
+#     dc.DimcatConfig(
+#         "PrevalenceAnalyzer",
+#         columns=["root_per_globalkey", "fifths_over_local_tonic"],
+#         index=["corpus", "piece"],
+#     )
+# )
+# print(f"Shape: {root_per_globalkey.shape}")
 
-# %% [markdown]
-# ### Document frequencies of the tokens
+# %% [raw] jupyter={"outputs_hidden": false}
+# fig = utils.plot_document_frequency(chord_reduced)
+# save_figure_as(fig, "document_frequency_of_chord_tones")
+# fig
 
-# %%
-fig = utils.plot_document_frequency(chord_reduced)
-save_figure_as(fig, "document_frequency_of_chord_tones")
-fig
+# %% [raw] jupyter={"outputs_hidden": false}
+# utils.plot_document_frequency(numerals, info="numerals")
 
-# %%
-utils.plot_document_frequency(numerals, info="numerals")
+# %% [raw] jupyter={"outputs_hidden": false}
+# utils.plot_document_frequency(roots, info="roots")
 
-# %%
-utils.plot_document_frequency(roots, info="roots")
+# %% [raw] jupyter={"outputs_hidden": false}
+# utils.plot_document_frequency(
+#     root_per_globalkey, info="root relative to global tonic"
+# )
 
-# %%
-utils.plot_document_frequency(root_per_globalkey, info="root relative to global tonic")
-
-# %% [markdown]
+# %% [raw] jupyter={"outputs_hidden": false}
 # ## Principal Component Analyses
 
-# %%
-# chord_reduced.query("piece in ['op03n12a', 'op03n12b']").dropna(axis=1, how='all')
+# %% [raw] jupyter={"outputs_hidden": false}
+# # chord_reduced.query("piece in ['op03n12a', 'op03n12b']").dropna(axis=1, how='all')
 
-# %%
-metadata = D.get_metadata()
-CORPUS_YEARS = utils.corpus_mean_composition_years(metadata)
-PIECE_YEARS = metadata.get_composition_years().rename("mean_composition_year")
-utils.plot_pca(
-    chord_reduced.relative,
-    info="chord-tone profiles of reduced chords",
-    color=PIECE_YEARS,
-)
+# %% [raw] jupyter={"outputs_hidden": false}
+# metadata = D.get_metadata()
+# CORPUS_YEARS = utils.corpus_mean_composition_years(metadata)
+# PIECE_YEARS = metadata.get_composition_years().rename("mean_composition_year")
+# utils.plot_pca(
+#     chord_reduced.relative,
+#     info="chord-tone profiles of reduced chords",
+#     color=PIECE_YEARS,
+# )
 
-# %%
-utils.plot_pca(
-    chord_reduced.combine_results("corpus").relative,
-    info="chord-tone profiles of reduced chords",
-    color=CORPUS_YEARS,
-    size=5,
-)
+# %% [raw] jupyter={"outputs_hidden": false}
+# utils.plot_pca(
+#     chord_reduced.combine_results("corpus").relative,
+#     info="chord-tone profiles of reduced chords",
+#     color=CORPUS_YEARS,
+#     size=5,
+# )
 
-# %%
-utils.plot_pca(
-    numerals.relative, info="numeral profiles of numerals", color=PIECE_YEARS
-)
+# %% [raw] jupyter={"outputs_hidden": false}
+# utils.plot_pca(
+#     numerals.relative, info="numeral profiles of numerals", color=PIECE_YEARS
+# )
 
-# %%
-utils.plot_pca(
-    numerals.combine_results("corpus").relative,
-    info="chord-tone profiles of numerals",
-    color=CORPUS_YEARS,
-    size=5,
-)
+# %% [raw] jupyter={"outputs_hidden": false}
+# utils.plot_pca(
+#     numerals.combine_results("corpus").relative,
+#     info="chord-tone profiles of numerals",
+#     color=CORPUS_YEARS,
+#     size=5,
+# )
 
-# %%
-utils.plot_pca(
-    roots.relative, info="root profiles of chord roots (local)", color=PIECE_YEARS
-)
+# %% [raw] jupyter={"outputs_hidden": false}
+# utils.plot_pca(
+#     roots.relative, info="root profiles of chord roots (local)", color=PIECE_YEARS
+# )
 
-# %%
-utils.plot_pca(
-    roots.combine_results("corpus").relative,
-    info="chord-tone profiles of chord roots (local)",
-    color=CORPUS_YEARS,
-    size=5,
-)
+# %% [raw] jupyter={"outputs_hidden": false}
+# utils.plot_pca(
+#     roots.combine_results("corpus").relative,
+#     info="chord-tone profiles of chord roots (local)",
+#     color=CORPUS_YEARS,
+#     size=5,
+# )
 
-# %%
-utils.plot_pca(
-    root_per_globalkey.relative,
-    info="root profiles of chord roots (global)",
-    color=PIECE_YEARS,
-)
+# %% [raw] jupyter={"outputs_hidden": false}
+# utils.plot_pca(
+#     root_per_globalkey.relative,
+#     info="root profiles of chord roots (global)",
+#     color=PIECE_YEARS,
+# )
 
-# %%
-utils.plot_pca(
-    root_per_globalkey.combine_results("corpus").relative,
-    info="chord-tone profiles of chord roots (global)",
-    color=CORPUS_YEARS,
-    size=5,
-)
+# %% [raw] jupyter={"outputs_hidden": false}
+# utils.plot_pca(
+#     root_per_globalkey.combine_results("corpus").relative,
+#     info="chord-tone profiles of chord roots (global)",
+#     color=CORPUS_YEARS,
+#     size=5,
+# )
